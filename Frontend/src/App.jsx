2c31db55812f9a642d27da1f4b69e91240b04ef3
Namrata Sahani <namratasahani@Nmaratas-MacBook.local>
2026-05-22 10:37:29 +0530

    Setup fronend and Added UI home page

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Banner from './component/Banner'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/component/Banner.jsx
@@ -0,0 +1,12 @@
+function Banner() {
+    return (
+        <>
+            <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+                <div className="w-full md:w-1/2 ">Left Side</div>
+                <div className="w-full md:w-1/2">Right Side</div>
+            </div>
+        </>
+    )
+}
+
+export default Banner
