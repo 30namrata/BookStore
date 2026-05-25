3191072f4dafc4c079df8b9b6ac77e63375eb98e
Namrata Sahani <namratasahani@Nmaratas-MacBook.local>
2026-05-25 10:10:29 +0530

    home page is ready and committed

--- Frontend/src/component/Freebook.jsx
+++ Frontend/src/component/Freebook.jsx
@@ -3,6 +3,7 @@ import list from "../../public/list.json";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Cards from "./Cards";
 
 function Freebook() {
     const SliderComponent = Slider.default || Slider;
@@ -12,8 +13,8 @@ function Freebook() {
         dots: true,
         infinite: false,
         speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         initialSlide: 0,
         responsive: [
             {
@@ -48,40 +49,21 @@ function Freebook() {
     return (
         <>
             <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-                <h1 className="text-2xl font-bold pb-2" >Free Offered Courses</h1>
-                <p className="pb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-            </div>
+                <div>
+                    <h1 className="text-2xl font-bold pb-2" >Free Offered Courses</h1>
+                    <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                </div>
 
-            <div>
+
                 <SliderComponent {...settings}>
-                    <div>
-                        <h3>1</h3>
-                    </div>
-                    <div>
-                        <h3>2</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
-                    </div>
-                    <div>
-                        <h3>7</h3>
-                    </div>
-                    <div>
-                        <h3>8</h3>
-                    </div>
-                    <div>
-                        <h3>9</h3>
-                    </div>
+                    {
+                        bookList.map((item) => {
+                            return (<Cards item={item} key={item.id} />)
+                        })
+                    }
+
                 </SliderComponent>
+
             </div>
 
         </>
